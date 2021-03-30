78ebd14175f77020e716d578eda6032885e46a31
hideo-Hirooka <hideo.hirooka@gmail.com>
2021-03-30 22:26:45 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Get Wet!'));
+app.get('/', (req, res) => res.send('Mahalo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
